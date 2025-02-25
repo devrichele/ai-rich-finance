0a2517fd0267bbbd92aa3f42208c403e46cba6c3
Richele <168489035+devrichele@users.noreply.github.com>
2025-02-25 22:29:23 +0000

    feat: implementando chatbot

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,27 +1,27 @@
-import { openai } from '@ai-sdk/openai';
-import { fireworks } from '@ai-sdk/fireworks';
+import { google } from "@ai-sdk/google";
+import { fireworks } from "@ai-sdk/fireworks";
 import {
   customProvider,
   extractReasoningMiddleware,
   wrapLanguageModel,
-} from 'ai';
+} from "ai";
 
-export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
+export const DEFAULT_CHAT_MODEL: string = "gemini-2.0-flash-001";
 
 export const myProvider = customProvider({
   languageModels: {
-    'chat-model-small': openai('gpt-4o-mini'),
-    'chat-model-large': openai('gpt-4o'),
-    'chat-model-reasoning': wrapLanguageModel({
-      model: fireworks('accounts/fireworks/models/deepseek-r1'),
-      middleware: extractReasoningMiddleware({ tagName: 'think' }),
+    "chat-model-small": google("gemini-2.0-flash-001"),
+    "chat-model-large": google("gpt-4o"),
+    "chat-model-reasoning": wrapLanguageModel({
+      model: fireworks("accounts/fireworks/models/deepseek-r1"),
+      middleware: extractReasoningMiddleware({ tagName: "think" }),
     }),
-    'title-model': openai('gpt-4-turbo'),
-    'artifact-model': openai('gpt-4o-mini'),
+    "title-model": google("gpt-4-turbo"),
+    "artifact-model": google("gpt-4o-mini"),
   },
   imageModels: {
-    'small-model': openai.image('dall-e-2'),
-    'large-model': openai.image('dall-e-3'),
+    "small-model": google.image("dall-e-2"),
+    "large-model": google.image("dall-e-3"),
   },
 });
 
@@ -33,18 +33,18 @@ interface ChatModel {
 
 export const chatModels: Array<ChatModel> = [
   {
-    id: 'chat-model-small',
-    name: 'Small model',
-    description: 'Small model for fast, lightweight tasks',
+    id: "chat-model-small",
+    name: "Small model",
+    description: "Small model for fast, lightweight tasks",
   },
   {
-    id: 'chat-model-large',
-    name: 'Large model',
-    description: 'Large model for complex, multi-step tasks',
+    id: "chat-model-large",
+    name: "Large model",
+    description: "Large model for complex, multi-step tasks",
   },
   {
-    id: 'chat-model-reasoning',
-    name: 'Reasoning model',
-    description: 'Uses advanced reasoning',
+    id: "chat-model-reasoning",
+    name: "Reasoning model",
+    description: "Uses advanced reasoning",
   },
 ];
